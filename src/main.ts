dfb7c0b883afc9e782e69197bf1c9cdbbdb943d6
Julien Prugne <julien@webeleon.dev>
2024-06-17 01:02:03 +0200

    chore: add helmet for basic security

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { appConfig } from './configurations/app.config';
 import { ConfigType } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
 
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   await app.listen(config.port);
