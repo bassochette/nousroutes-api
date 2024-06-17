1ba3a72b9c60aeaa1b842fdffb74908ea57bb5f2
Julien Prugne <julien@webeleon.dev>
2024-06-17 09:29:38 +0200

    docs: setup swagger automatic generation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { appConfig } from './configurations/app.config';
 import { ConfigType } from '@nestjs/config';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.use(helmet());
+
+  const swaggerCongig = new DocumentBuilder()
+    .setTitle('WeRoad API')
+    .setDescription('Technical case Julien Prugne julien@webeleon.dev')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerCongig);
+  SwaggerModule.setup('swagger', app, document);
 
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
   await app.listen(config.port);
